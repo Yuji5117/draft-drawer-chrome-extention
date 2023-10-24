7d208d065dfddb9a281340cccb73174f31f843fd
Yuji Nakamura <ny5117@icloud.com>
2023-10-24 20:41:10 +0900

    themeを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#028536",
+      red: "#F54F4F",
+    },
     extend: {},
   },
   plugins: [],
 };
-
