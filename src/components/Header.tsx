3fa452f7fefbca9e07a89e22fd5d4655e8890619
Yuji Nakamura <ny5117@icloud.com>
2023-10-25 18:21:09 +0900

    検索フィールドコンポーネントを実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
+import { SearchInputField } from "./SearchInputField";
+import { useForm } from "react-hook-form";
+
 export const Header = () => {
-  return <div className="bg-primary h-12">
-    {/* Search Bar */}
-    {/* Add Button */}
-  </div>;
+  const { register } = useForm();
+  return (
+    <div className="bg-primary h-12 text-sm">
+      <div className="h-full flex justify-between items-center mx-6">
+        <div>
+          <SearchInputField registration={register("query")} />
+        </div>
+        {/* Add Button */}
+        <div>New Template</div>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/SearchInputField.tsx
@@ -0,0 +1,20 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type SearchInputFieldProps = React.HtmlHTMLAttributes<HTMLInputElement> & {
+  placeholder?: string;
+  registration: Partial<UseFormRegisterReturn>;
+};
+
+export const SearchInputField = ({
+  placeholder = "Search template...",
+  registration,
+}: SearchInputFieldProps) => {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      className="bg-[#A2C6A1] bg-opacity-80 text-white placeholder-white placeholder-opacity-50 outline-none w-60 h-9 pl-3 rounded-sm"
+      {...registration}
+    />
+  );
+};
