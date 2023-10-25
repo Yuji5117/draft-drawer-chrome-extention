7c5e63472d57900ba9047eb4bb746bb87f84bd94
Yuji Nakamura <ny5117@icloud.com>
2023-10-25 18:43:40 +0900

    検索アイコンを実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
-import { SearchInputField } from "./SearchInputField";
 import { useForm } from "react-hook-form";
+
+import { SearchIcon } from "./SearchIcon";
+import { SearchInputField } from "./SearchInputField";
 
 export const Header = () => {
   const { register } = useForm();
   return (
     <div className="bg-primary h-12 text-sm">
       <div className="h-full flex justify-between items-center mx-6">
-        <div>
+        <div className="flex">
+          <SearchIcon />
           <SearchInputField registration={register("query")} />
         </div>
         {/* Add Button */}
---
+++ src/components/SearchIcon.tsx
@@ -0,0 +1,9 @@
+import { CiSearch } from 'react-icons/ci';
+
+export const SearchIcon = () => {
+  return (
+    <div className="bg-[#A2C6A1] bg-opacity-80 w-9 h-9 rounded-l-sm flex items-center justify-center">
+      <CiSearch size={23} color="white" />
+    </div>
+  );
+};
