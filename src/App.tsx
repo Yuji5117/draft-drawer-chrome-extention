74d5aa7d60f4dca6f6cf4e077c422a88f79d282d
Yuji Nakamura <ny5117@icloud.com>
2023-10-25 18:20:44 +0900

    cssを修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="w-[400px] y-[700px]">
+    <div className="w-[700px] y-[400px]">
       {/* Header */}
       <Header />
       {/* Dashboard */}
