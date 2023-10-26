d156ecbdc68d08169a9be02ce37c6972e2fb05bd
Yuji Nakamura <ny5117@icloud.com>
2023-10-26 21:02:18 +0900

    Textコンポーネントを実装

---
+++ src/components/Text.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const variants = {
+  default: "text-black",
+  white: "text-white",
+};
+
+const sizes = {
+  sm: "text-sm",
+  md: "text-md",
+  lg: "text-lg",
+};
+
+type TextProps = {
+  children: React.ReactNode;
+  variant?: keyof typeof variants;
+  size?: keyof typeof sizes;
+};
+
+export const Text = ({
+  children,
+  variant = "default",
+  size = "md",
+}: TextProps) => {
+  return <p className={`${variant} ${size}`}>{children}</p>;
+};
