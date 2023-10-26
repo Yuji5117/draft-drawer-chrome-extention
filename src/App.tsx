329d3310683a7ec7d9d34e61eed8c4313ec7f48f
Yuji Nakamura <ny5117@icloud.com>
2023-10-26 22:10:52 +0900

    templateのリストを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import { Header } from "./components/Header";
+import { TemplateItem } from "./components/TemplateItem";
+
+const templates = [
+  { id: 1, title: "test" },
+  { id: 2, title: "hello" },
+  { id: 3, title: "why" },
+  { id: 4, title: "testtesttesttest" },
+  { id: 5, title: "mean" },
+  { id: 6, title: "sorry" },
+  { id: 7, title: "cool" },
+  { id: 8, title: "bad" },
+];
 
 function App() {
   return (
-    <div className="w-[700px] y-[400px]">
+    <div className="w-[700px] h-[400px] bg-white">
       {/* Header */}
       <Header />
       {/* Dashboard */}
+      <main className="w-full">
+        <div className="flex mx-6 py-3">
+          <div className="w-[45%] h-[315px] overflow-scroll">
+            <ul className="mr-2">
+              {templates.map((template) => (
+                <li className="h-10" key={template.id}>
+                  <TemplateItem title={template.title} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="bg-rose-400 w-[55%] h-[315px] "></div>
+        </div>
+      </main>
     </div>
   );
 }
