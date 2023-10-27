d0a08a45bde4373bb9613b90a3edbde6d85b9ff2
Yuji Nakamura <ny5117@icloud.com>
2023-10-27 20:23:49 +0900

    DOMを修正

--- src/components/TemplateContent.tsx
+++ src/components/TemplateContent.tsx
@@ -2,18 +2,14 @@ import { Button } from "./Button";
 
 export const TemplateContent = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mx-5">
       <div className="h-16 flex items-center justify-end space-x-3">
-        <div>
-          <Button variant="primary" size="sm">
-            Edit
-          </Button>
-        </div>
-        <div className="pr-5">
-          <Button variant="danger" size="sm">
-            Delete
-          </Button>
-        </div>
+        <Button variant="primary" size="sm">
+          Edit
+        </Button>
+        <Button variant="danger" size="sm">
+          Delete
+        </Button>
       </div>
 
       <div></div>
