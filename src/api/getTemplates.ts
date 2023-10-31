3a05543c4f67a83b481eb1d84d5129f556fc201e
Yuji Nakamura <ny5117@icloud.com>
2023-10-31 21:44:59 +0900

    react-queryを使ってtemplatesを取得するように実装

--- src/api/getTemplates.ts
+++ src/api/getTemplates.ts
@@ -1,6 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+
 import { getAllDocs } from "@/libs/firebase";
 import { Template } from "@/types";
 
 export const getTemplates = async (): Promise<Template[]> => {
   return getAllDocs("templates");
+};
+
+export const useTemplates = () => {
+  return useQuery({
+    queryKey: ["templates"],
+    queryFn: () => getTemplates(),
+  });
 };
