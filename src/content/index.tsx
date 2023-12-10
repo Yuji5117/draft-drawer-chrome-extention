20f90f7c43366c1bea633550edc3c38a850d6ca5
Yuji Nakamura <ny5117@icloud.com>
2023-12-10 18:17:35 +0900

    選択したテンプレートを挿入できるように実装

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -10,17 +10,16 @@ document.addEventListener(
       event.target instanceof HTMLTextAreaElement
     ) {
       const container = document.createElement("my-extension-root");
+      container.id = "myExtensionRoot";
       document.body.appendChild(container);
 
-      const rect = event.target.getBoundingClientRect();
+      const rect = event.target?.getBoundingClientRect();
       container.style.position = "absolute";
-      container.style.left = rect.left + window.scrollX + 10 + "px"; // カーソルの少し右側に配置
-      container.style.top = rect.top + window.scrollY + 10 + "px";
+      container.style.left = rect.left + window.scrollX + 50 + "px"; // カーソルの少し右側に配置
+      container.style.top = rect.top + window.scrollY + 50 + "px";
+      container.style.zIndex = "2147483550";
 
       createRoot(container).render(<TemplateList />);
-      event.target?.addEventListener("blur", function () {
-        container.remove();
-      });
     }
   },
   true
