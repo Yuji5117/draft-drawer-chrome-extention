66ddaf5f232db377e0a62324fe3aa7402f27551c
Yuji Nakamura <ny5117@icloud.com>
2025-08-30 12:23:32 +1200

    テンプレート削除時にキャッシュにも反映させるように修正

--- src/api/deleteTemplate.ts
+++ src/api/deleteTemplate.ts
@@ -2,10 +2,19 @@ import { useMutation } from "@tanstack/react-query";
 
 import { deleteDocFn } from "@/libs/firebase";
 import { queryClient } from "@/libs/react-query";
+import { storage } from "@/libs/storage";
 import { Template } from "@/types";
 
-export const deleteTemplate = async (id: string): Promise<Template> => {
-  return await deleteDocFn("templates", id);
+export const deleteTemplate = async (id: string): Promise<void> => {
+  await deleteDocFn("templates", id);
+
+  const currentCachedTemplates = await storage.get("templates");
+  if (currentCachedTemplates) {
+    const updatedTemplates = currentCachedTemplates.filter(
+      (template) => template.id !== id
+    );
+    await storage.set("templates", updatedTemplates);
+  }
 };
 
 export const useDeleteTemplate = () => {
