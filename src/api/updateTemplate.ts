132ac0d4e6546ea69e40c1810792b459e71f1601
Yuji Nakamura <ny5117@icloud.com>
2025-08-30 12:17:40 +1200

    テンプレート更新時に更新内容をキャッシュに反映

--- src/api/updateTemplate.ts
+++ src/api/updateTemplate.ts
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { updateDocFn } from "@/libs/firebase";
 import { queryClient } from "@/libs/react-query";
 import { Template } from "@/types";
+import { storage } from "@/libs/storage";
 
 type UpdateTemplateDTO = {
   data: { title?: string; content?: string };
@@ -15,7 +16,21 @@ export const updateTemplate = async ({
   data,
   templateId,
 }: UpdateTemplateDTO): Promise<Template> => {
-  return updateDocFn<UpdateTemplateData>("templates", templateId, data);
+  const updatedTemplate = await updateDocFn<UpdateTemplateData>(
+    "templates",
+    templateId,
+    data
+  );
+
+  const currentCachedTemplates = await storage.get("templates");
+  if (currentCachedTemplates) {
+    const updatedTemplates = currentCachedTemplates.map(template =>
+      template.id === templateId ? updatedTemplate : template
+    );
+    await storage.set("templates", updatedTemplates);
+  }
+
+  return updatedTemplate;
 };
 
 export const useUpdateTemplate = () => {
